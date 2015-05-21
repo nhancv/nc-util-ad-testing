1801f8fc5823483ac31420d4326a1bc9899f7c51
cvnhan <caovannhan2002@gmail.com>
2015-05-21 16:51:46 +0700

    Simple Espresso test

--- app/src/androidTest/java/cvnhan/android/androidtest/MainActivityTest.java
+++ app/src/androidTest/java/cvnhan/android/androidtest/MainActivityTest.java
@@ -3,6 +3,13 @@ package cvnhan.android.androidtest;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 /**
  * Created by cvnhan on 21-May-15.
  */
@@ -11,5 +18,20 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2 <MainActi
 
     public MainActivityTest() {
         super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        getActivity();
+    }
+
+    public void testCase(){
+        onView(withText("Hello world!")).check(matches(isDisplayed()));
+    }
+    public void testCase2() {
+        onView(withText("aklsdfj")).check(matches(isDisplayed()));
+        onView(withText("aklsdfj")).perform(click());
+        onView(withId(R.id.btn)).check(matches(withText("Hello world!")));
     }
 }
--- app/src/main/java/cvnhan/android/androidtest/MainActivity.java
+++ app/src/main/java/cvnhan/android/androidtest/MainActivity.java
@@ -2,9 +2,12 @@ package cvnhan.android.androidtest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import javax.inject.Inject;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import timber.log.Timber;
 
 
@@ -17,12 +20,24 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         Timber.tag(getClass().getSimpleName());
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
         ((App) getApplication()).getComponent().inject(this);
 
-        Timber.e("begin dagger 2 for vehicle");
-        vehicle.getRPMVehicle();
-        vehicle.initRPM();
-        vehicle.getRPMVehicle();
-        Timber.e("end dagger 2 for vehicle");
+//        Timber.e("begin dagger 2 for vehicle");
+//        vehicle.getRPMVehicle();
+//        vehicle.initRPM();
+//        vehicle.getRPMVehicle();
+//        Timber.e("end dagger 2 for vehicle");
+    }
+
+    @OnClick(R.id.btn)
+    public void clickBtn(Button btn) {
+        btn.setText("Hello world!");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.reset(this);
     }
 }
